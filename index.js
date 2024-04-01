d13d8fc8146daf8f8974d64aa41866fc1bb69bcb
Michael <oliengo.michael@gmail.com>
2024-04-01 18:11:05 +0300

    complete Functions: Continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// lab-1
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+
+// Lab-2
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+
+}
+
+// lab-3
+function wrapAdjective(string="*") {
+    return function(parameter="special") {
+        return `You are ${string}${parameter}${string}!`
+    }
+}
